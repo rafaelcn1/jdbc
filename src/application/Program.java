0e5682f551415375ea94442e1e1c36939436c8a9
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-29 16:49:33 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,19 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "Books");
 		System.out.println(department.toString());
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller.toString());
 	}
 
 }
