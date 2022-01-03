09e9b2d6724a5a63a66c3eff1982f5e7d59b4bd1
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-03 14:41:40 -0300

    Criação do SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,9 +12,10 @@ public class Program {
 
 		Department department = new Department(1, "Books");
 		System.out.println(department.toString());
-		
+
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller.toString());
 	}
 
